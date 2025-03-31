a54c39c1efddea01376318ba0f3e92ab8ef3a72d
Prantik Barik <prantikbarik1234@gmail.com>
2025-03-31 10:32:19 +0530

    added name tag

--- app/page.js
+++ app/page.js
@@ -80,14 +80,7 @@ export default function Home() {
         <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold gradient-title pb-6 flex flex-col">
           Streamline Your Workflow <br />
           <span className="flex mx-auto gap-3 sm:gap-4 items-center">
-            with
-            <Image
-              src={"/logo2.png"}
-              alt="Zscrum Logo"
-              width={400}
-              height={80}
-              className="h-14 sm:h-24 w-auto object-contain"
-            />
+            with SPRINTZ
           </span>
         </h1>
         <p className="text-xl text-gray-300 mb-10 max-w-3xl mx-auto">
